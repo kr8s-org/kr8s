1a89dd25a74113df039747faeb23f1f8986f51de
Jacob Tomlinson <jacob@tomlinson.email>
2023-03-23 14:54:46 +0000

    Add poetry-dynamic-versioning

--- kr8s/__init__.py
+++ kr8s/__init__.py
@@ -2,3 +2,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023, Dask Developers, Yuvi Panda, Anaconda Inc, NVIDIA
 # SPDX-License-Identifier: BSD 3-Clause License
 from pykube import HTTPClient, KubeConfig, all  # noqa
+
+__version__ = "0.0.0"
