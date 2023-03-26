a28f18457b12454fc138d1c7812664f9b558cda7
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2023-03-26 20:26:20 +0100

    Implement initial api and auth objects (#6)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kr8s/conftest.py
+++ kr8s/conftest.py
@@ -1,10 +1,19 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023, Dask Developers, Yuvi Panda, Anaconda Inc, NVIDIA
 # SPDX-License-Identifier: BSD 3-Clause License
+import asyncio
 import os
+import socket
+import subprocess
 import time
+from contextlib import closing
 
 import pytest
 from pytest_kind.cluster import KindCluster
+
+
+def check_socket(host, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        return sock.connect_ex((host, port)) == 0
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -30,3 +39,17 @@ def k8s_cluster(request):
     del os.environ["KUBECONFIG"]
     if not request.config.getoption("keep_cluster"):
         kind_cluster.delete()
+
+
+@pytest.fixture
+async def kubectl_proxy(k8s_cluster):
+    proxy = subprocess.Popen(
+        [k8s_cluster.kubectl_path, "proxy"],
+        env={**os.environ, "KUBECONFIG": str(k8s_cluster.kubeconfig_path)},
+    )
+    host = "localhost"
+    port = 8001
+    while not check_socket(host, port):
+        await asyncio.sleep(0.1)
+    yield f"http://{host}:{port}"
+    proxy.kill()
