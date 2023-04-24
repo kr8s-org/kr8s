983674578453456b749aaaf531db1c08f66d3da4
Jacob Tomlinson <jacob@tomlinson.email>
2023-04-24 09:42:13 +0100

    Fix unreachable assertion

--- kr8s/tests/test_api.py
+++ kr8s/tests/test_api.py
@@ -11,7 +11,7 @@ from kr8s.objects import Pod
 async def test_factory_bypass():
     with pytest.raises(ValueError, match="kr8s.api()"):
         _ = kr8s.Api()
-        assert not kr8s.Api._instances
+    assert not kr8s.Api._instances
     _ = kr8s.api()
     assert kr8s.Api._instances
 
