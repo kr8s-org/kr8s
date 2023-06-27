6a87530af85ba1f3ea120281d9b193559976bce3
Jacob Tomlinson <jacob@tomlinson.email>
2023-06-27 12:31:44 +0100

    Replace aiofiles with anyio.Path

--- kr8s/tests/test_asyncio.py
+++ kr8s/tests/test_asyncio.py
@@ -1,7 +1,10 @@
+import anyio
+
 from kr8s._asyncio import NamedTemporaryFile
 
 
 async def test_tempfiles():
     async with NamedTemporaryFile() as f:
         assert await f.exists()
+        assert isinstance(f, anyio.Path)
     assert not await f.exists()
