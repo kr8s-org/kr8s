77d6b528919283328e4c2df6f05c664b7e09c867
Jacob Tomlinson <jacob@tomlinson.email>
2023-11-28 13:49:03 +0000

    For sync port forwards start a background thread

--- kr8s/portforward.py
+++ kr8s/portforward.py
@@ -1,3 +1,5 @@
+import threading
+
 from ._io import sync
 from ._portforward import PortForward as _PortForward
 
@@ -5,3 +7,14 @@ from ._portforward import PortForward as _PortForward
 @sync
 class PortForward(_PortForward):
     __doc__ = _PortForward.__doc__
+    _bg_thread = None
+
+    def start(self):
+        """Start a background thread with the port forward running."""
+        self._bg_thread = threading.Thread(target=self.run_forever, daemon=True)
+        self._bg_thread.start()
+
+    def stop(self):
+        """Stop the background thread."""
+        self.server.close()
+        self.server = None
