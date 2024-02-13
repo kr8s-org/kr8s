2b5833ecdfdfb8de5dabdbf4dccb40c97675eff6
Jacob Tomlinson <jacob@tomlinson.email>
2024-02-13 16:58:21 +0000

    Only load service account of kubeconfig was not found

--- kr8s/_auth.py
+++ kr8s/_auth.py
@@ -58,7 +58,7 @@ class KubeAuth:
             else:
                 if self._kubeconfig is not False:
                     await self._load_kubeconfig()
-                if self._serviceaccount:
+                if self._serviceaccount and not self.server:
                     await self._load_service_account()
             if not self.server:
                 raise ValueError("Unable to find valid credentials")
