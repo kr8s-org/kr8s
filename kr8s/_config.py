dc52c012da22ded23af42bf5338512a7380946f7
Jacob Tomlinson <jacob@tomlinson.email>
2024-04-03 17:45:59 +0100

    Implement more config manipulation

--- kr8s/_config.py
+++ kr8s/_config.py
@@ -1,15 +1,15 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024, Kr8s Developers (See LICENSE for list)
 # SPDX-License-Identifier: BSD 3-Clause License
+from typing import Dict, List
+
 import anyio
 import yaml
 
-# TODO Implement raw
 # TODO Implement set
 # TODO Implement unset
 # TODO Implement set cluster
 # TODO Implement delete cluster
 # TODO Implement set context
-# TODO Implement rename context
 # TODO Implement delete context
 # TODO Implement set user
 # TODO Implement delete user
@@ -32,36 +32,66 @@ class KubeConfigSet(object):
             await config.save()
 
     @property
-    def raw(self):
-        raise NotImplementedError("raw not implemented")
+    def raw(self) -> Dict:
+        """Merge all kubeconfig data into a single kubeconfig."""
+        data = {
+            "apiVersion": "v1",
+            "kind": "Config",
+            "preferences": self.preferences,
+            "clusters": self.clusters,
+            "users": self.users,
+            "contexts": self.contexts,
+            "current-context": self.current_context,
+        }
+        if self.extensions:
+            data["extensions"] = self.extensions
+        return data
 
     @property
-    def current_context(self):
+    def current_context(self) -> str:
         """Return the current context from the first kubeconfig.
 
         Context configuration from multiples files are ignored.
         """
         return self._configs[0].current_context
 
-    async def use_context(self, context: str):
+    async def use_context(self, context: str) -> None:
         """Set the current context."""
-        await self._configs[0].use_context(context)
+        if context not in [c["name"] for c in self.contexts]:
+            raise ValueError(f"Context {context} not found")
+        await self._configs[0].use_context(context, allow_unknown=True)
+
+    async def rename_context(self, old: str, new: str) -> None:
+        """Rename a context."""
+        for config in self._configs:
+            if old in [c["name"] for c in config.contexts]:
+                await config.rename_context(old, new)
+                if self.current_context == old:
+                    await self.use_context(new)
+                return
+        raise ValueError(f"Context {old} not found")
 
     @property
-    def preferences(self):
-        raise NotImplementedError("preferences not implemented")
+    def preferences(self) -> Dict:
+        return self._configs[0].preferences
 
     @property
-    def clusters(self):
+    def clusters(self) -> List[Dict]:
         return [cluster for config in self._configs for cluster in config.clusters]
 
     @property
-    def users(self):
+    def users(self) -> List[Dict]:
         return [user for config in self._configs for user in config.users]
 
     @property
-    def contexts(self):
+    def contexts(self) -> List[Dict]:
         return [context for config in self._configs for context in config.contexts]
+
+    @property
+    def extensions(self) -> List[Dict]:
+        return [
+            extension for config in self._configs for extension in config.extensions
+        ]
 
 
 class KubeConfig(object):
@@ -78,36 +108,53 @@ class KubeConfig(object):
 
         return f().__await__()
 
-    async def save(self):
+    async def save(self) -> None:
         async with self.__write_lock:
             async with await anyio.open_file(self.path, "w") as fh:
                 await fh.write(yaml.safe_dump(self._raw))
 
     @property
-    def current_context(self):
+    def current_context(self) -> str:
         return self._raw["current-context"]
 
-    async def use_context(self, context: str):
+    async def use_context(self, context: str, allow_unknown: bool = False) -> None:
         """Set the current context."""
+        if not allow_unknown and context not in [c["name"] for c in self.contexts]:
+            raise ValueError(f"Context {context} not found")
         self._raw["current-context"] = context
         await self.save()
 
+    async def rename_context(self, old: str, new: str) -> None:
+        """Rename a context."""
+        for context in self._raw["contexts"]:
+            if context["name"] == old:
+                context["name"] = new
+                if self.current_context == old:
+                    await self.use_context(new)
+                await self.save()
+                return
+        raise ValueError(f"Context {old} not found")
+
     @property
-    def raw(self):
+    def raw(self) -> Dict:
         return self._raw
 
     @property
-    def preferences(self):
+    def preferences(self) -> List[Dict]:
         return self._raw["preferences"]
 
     @property
-    def clusters(self):
+    def clusters(self) -> List[Dict]:
         return self._raw["clusters"]
 
     @property
-    def users(self):
+    def users(self) -> List[Dict]:
         return self._raw["users"]
 
     @property
-    def contexts(self):
+    def contexts(self) -> List[Dict]:
         return self._raw["contexts"]
+
+    @property
+    def extensions(self) -> List[Dict]:
+        return self._raw["extensions"]
--- kr8s/tests/test_config.py
+++ kr8s/tests/test_config.py
@@ -1,12 +1,26 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024, Kr8s Developers (See LICENSE for list)
 # SPDX-License-Identifier: BSD 3-Clause License
+from tempfile import NamedTemporaryFile
+
+import pytest
+
 from kr8s._config import KubeConfig, KubeConfigSet
 
 
-async def test_load_kubeconfig(k8s_cluster):
-    path = str(k8s_cluster.kubeconfig_path)
-    config = await KubeConfig(path)
-    assert config.path == path
+@pytest.fixture
+def temp_kubeconfig(k8s_cluster):
+    """Create a temporary kubeconfig by copying the one from k8s_cluster."""
+    with open(k8s_cluster.kubeconfig_path, "rb") as f:
+        kubeconfig = f.read()
+    with NamedTemporaryFile() as f:
+        f.write(kubeconfig)
+        f.flush()
+        yield f.name
+
+
+async def test_load_kubeconfig(temp_kubeconfig):
+    config = await KubeConfig(temp_kubeconfig)
+    assert config.path == temp_kubeconfig
     assert config._raw
     assert "current-context" in config._raw
     assert config.current_context
@@ -16,11 +30,10 @@ async def test_load_kubeconfig(k8s_cluster):
     assert len(config.contexts) > 0
 
 
-async def test_load_kubeconfig_set(k8s_cluster):
-    path = str(k8s_cluster.kubeconfig_path)
-    configs = await KubeConfigSet(path)
+async def test_load_kubeconfig_set(temp_kubeconfig):
+    configs = await KubeConfigSet(temp_kubeconfig)
     assert len(configs._configs) == 1
-    assert configs._configs[0].path == path
+    assert configs._configs[0].path == temp_kubeconfig
     assert configs._configs[0]._raw
     assert "current-context" in configs._configs[0]._raw
     assert configs.current_context
@@ -28,3 +41,23 @@ async def test_load_kubeconfig_set(k8s_cluster):
     assert "name" in configs.clusters[0]
     assert len(configs.users) > 0
     assert len(configs.contexts) > 0
+
+
+@pytest.mark.parametrize("cls", [KubeConfig, KubeConfigSet])
+async def test_rename_context(temp_kubeconfig, cls):
+    config = await cls(temp_kubeconfig)
+    context = config.current_context
+    new_context = f"{context}-new"
+    assert context in [c["name"] for c in config.contexts]
+    await config.rename_context(context, new_context)
+    # Load again from disk to ensure the change was saved
+    config = await cls(temp_kubeconfig)
+    assert new_context in [c["name"] for c in config.contexts]
+    assert context not in [c["name"] for c in config.contexts]
+    assert config.current_context == new_context
+    await config.rename_context(new_context, context)
+    assert new_context not in [c["name"] for c in config.contexts]
+    assert context in [c["name"] for c in config.contexts]
+    assert config.current_context == context
+    with pytest.raises(ValueError):
+        await config.rename_context("foobar", new_context)
