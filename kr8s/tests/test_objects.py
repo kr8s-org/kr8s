7fcd481231bf8ebfccb2dc8635073e1c9dca6caa
Jacob Tomlinson <jacob@tomlinson.email>
2024-04-26 13:12:48 +0100

    Test annotate

--- kr8s/tests/test_objects.py
+++ kr8s/tests/test_objects.py
@@ -416,7 +416,11 @@ async def test_pod_annotate(example_pod_spec):
     pod = await Pod(example_pod_spec)
     await pod.create()
     await pod.annotate({"foo": "bar"})
+    await pod.annotate(fizz="buzz")
     assert "foo" in pod.annotations
+    assert "fizz" in pod.annotations
+    with pytest.raises(ValueError):
+        await pod.annotate({})
     await pod.delete()
 
 
