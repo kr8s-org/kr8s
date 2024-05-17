f8fb24796955e76243ad7d6c3dff6be9812a7366
Jacob Tomlinson <jacob@tomlinson.email>
2024-05-17 10:55:24 +0100

    Add kubectl-ng config get-[clusters|users]

--- examples/kubectl-ng/kubectl_ng/_config.py
+++ examples/kubectl-ng/kubectl_ng/_config.py
@@ -1,8 +1,13 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024, Kr8s Developers (See LICENSE for list)
 # SPDX-License-Identifier: BSD 3-Clause License
+import rich.table
 import typer
+from rich import box
+from rich.console import Console
 
 import kr8s
+
+console = Console()
 
 config = typer.Typer(
     no_args_is_help=True,
@@ -18,3 +23,35 @@ def config_current_context():
         typer.echo(kr8s.api().auth.kubeconfig.current_context)
     except KeyError:
         typer.echo("error: current-context is not set")
+
+
+@config.command(name="get-clusters", help="Display clusters defined in the kubeconfig")
+def config_get_clusters():
+    """Display clusters defined in the kubeconfig."""
+    try:
+        clusters = [cluster["name"] for cluster in kr8s.api().auth.kubeconfig.clusters]
+    except:  # noqa
+        clusters = []
+
+    table = rich.table.Table(box=box.SIMPLE)
+    table.add_column("Name", style="magenta", no_wrap=True)
+    for cluster in clusters:
+        table.add_row(cluster)
+
+    console.print(table)
+
+
+@config.command(name="get-users", help="Display users defined in the kubeconfig")
+def config_get_users():
+    """Display users defined in the kubeconfig."""
+    try:
+        users = [user["name"] for user in kr8s.api().auth.kubeconfig.users]
+    except:  # noqa
+        users = []
+
+    table = rich.table.Table(box=box.SIMPLE)
+    table.add_column("Name", style="magenta", no_wrap=True)
+    for user in users:
+        table.add_row(user)
+
+    console.print(table)
