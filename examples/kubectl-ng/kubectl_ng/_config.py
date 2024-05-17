be117453fa1eaa7dd6786bc7be558c95825393a4
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2024-05-17 11:21:47 +0100

    Add kubectl-ng config get-contexts (#377)

--- examples/kubectl-ng/kubectl_ng/_config.py
+++ examples/kubectl-ng/kubectl_ng/_config.py
@@ -1,9 +1,12 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024, Kr8s Developers (See LICENSE for list)
 # SPDX-License-Identifier: BSD 3-Clause License
+from typing import Optional
+
 import rich.table
 import typer
 from rich import box
 from rich.console import Console
+from typing_extensions import Annotated
 
 import kr8s
 
@@ -23,6 +26,7 @@ def config_current_context():
         typer.echo(kr8s.api().auth.kubeconfig.current_context)
     except KeyError:
         typer.echo("error: current-context is not set")
+        raise typer.Exit(code=1)
 
 
 @config.command(name="get-clusters", help="Display clusters defined in the kubeconfig")
@@ -53,5 +57,37 @@ def config_get_users():
     table.add_column("Name", style="magenta", no_wrap=True)
     for user in users:
         table.add_row(user)
+
+    console.print(table)
+
+
+@config.command(name="get-contexts", help="Describe one or many contexts")
+def config_get_contexts(name: Annotated[Optional[str], typer.Argument()] = None):
+    """Display users defined in the kubeconfig."""
+    try:
+        kubeconfig = kr8s.api().auth.kubeconfig
+        contexts = kubeconfig.contexts
+    except:  # noqa
+        contexts = []
+
+    table = rich.table.Table(box=box.SIMPLE)
+    table.add_column("Current", no_wrap=True)
+    table.add_column("Name", style="magenta", no_wrap=True)
+    table.add_column("Cluster", style="blue", no_wrap=True)
+    table.add_column("Auth Info", style="orange3", no_wrap=True)
+    table.add_column("Namespace", style="yellow", no_wrap=True)
+    for context in contexts:
+        if name and context["name"] != name:
+            continue
+        table.add_row(
+            "*" if context["name"] == kubeconfig.current_context else "",
+            context["name"],
+            context["context"]["cluster"],
+            context["context"]["user"],
+            context["context"].get("namespace", ""),
+        )
+    if name and not table.rows:
+        typer.echo(f"error: context {name} not found")
+        raise typer.Exit(code=1)
 
     console.print(table)
