920f1805369407598263b0798bba006ab1744cff
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2024-05-17 10:58:16 +0100

    Add kubectl-ng config get-[clusters|users] (#376)

--- examples/kubectl-ng/kubectl_ng/tests/test_kng_config.py
+++ examples/kubectl-ng/kubectl_ng/tests/test_kng_config.py
@@ -8,8 +8,20 @@ import kr8s
 runner = CliRunner()
 
 
-def test_create_and_delete():
+def test_current_context():
     current_context = kr8s.api().auth.kubeconfig.current_context
     result = runner.invoke(app, ["config", "current-context"])
     assert result.exit_code == 0
     assert current_context in result.stdout
+
+
+def test_get_clusters(k8s_cluster):
+    result = runner.invoke(app, ["config", "get-clusters"])
+    assert result.exit_code == 0
+    assert k8s_cluster.name in result.stdout
+
+
+def test_get_users(k8s_cluster):
+    result = runner.invoke(app, ["config", "get-users"])
+    assert result.exit_code == 0
+    assert k8s_cluster.name in result.stdout
