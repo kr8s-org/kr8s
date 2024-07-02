fad544061e8860751a69bc8bfa576d7763bb7c59
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2024-07-02 16:21:07 +0100

    Add hook to sphinx-autoapi to fix sync wrapping (#430)

--- docs/conf.py
+++ docs/conf.py
@@ -72,5 +72,38 @@ intersphinx_mapping = {
 }
 
 
+def remove_async_property(app, what, name, obj, skip, options):
+    """Remove async property from sync wrapped methods.
+
+    Find all sync classes that inherit from async classes
+    and remove the async property from their wrapped methods.
+
+    Also skip public ``async_`` methods as they are only intended
+    to be used internally by other sync wrapped objects.
+    """
+    if what == "class" and (
+        # Infer that the class is a sync wrapped class if it is the kr8s.Api class,
+        # or is in kr8s.objects and inherits from a class in kr8s._objects.
+        name == "kr8s.Api"
+        or ("kr8s.objects" in name and obj.bases and "kr8s._objects" in obj.bases[0])
+        # FIXME: It would be better to just check if the class is decorated with @sync
+        # but sphinx-autoapi does not currently keep track of decorators
+        # See https://github.com/readthedocs/sphinx-autoapi/issues/459
+    ):
+        for child in obj.children:
+            if child.type == "method":
+                if child.name.startswith("async_"):
+                    child.properties.append("private-async-only")
+                    continue
+                if not child.name.startswith("_"):
+                    if "async" in child.properties:
+                        child.properties.remove("async")
+
+    if what == "method" and "private-async-only" in obj.properties:
+        skip = True
+    return skip
+
+
 def setup(app):
     app.add_css_file("css/custom.css")
+    app.connect("autoapi-skip-member", remove_async_property)
