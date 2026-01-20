bb2f832cff8effc77a3d4d932819d76944c71b7a
Daniel Goldman <danielgoldman4@gmail.com>
2026-01-19 23:49:45 -0500

    fix: typer doesn't support unions, so we parse the validate arg

--- examples/kubectl-ng/kubectl_ng/_create.py
+++ examples/kubectl-ng/kubectl_ng/_create.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2026, Kr8s Developers (See LICENSE for list)
 # SPDX-License-Identifier: BSD 3-Clause License
+from typing import Any
 
 import typer
 from rich.console import Console
@@ -27,6 +28,19 @@ console = Console()
 # TODO --windows-line-endings='false'
 
 
+def parse_validate(validate: str) -> ValidateOption:
+    normalised = validate.lower()
+    if normalised in {"true", "false"}:
+        return bool(normalised == "true")
+    elif normalised in {"strict", "warn", "ignore"}:
+        return normalised
+    else:
+        raise typer.BadParameter(
+            f"Invalid validate option: {validate}. It must be one of strict, warn, ignore.",
+            param_hint="validate",
+        )
+
+
 async def create(
     filename: str = typer.Option(
         "",
@@ -34,13 +48,14 @@ async def create(
         "-f",
         help="Filename, directory, or URL to files identifying the resources to create",
     ),
-    validate: ValidateOption = typer.Option(
+    validate: Any = typer.Option(
         "strict",
         "--validate",
         help="Must be one of strict, warn, ignore."
         ' "strict" will perform server side validation.'
         ' "warn" will warn about unknown or duplicate fields without blocking the request.'
         ' "ignore" will not perform any schema validation.',
+        parser=parse_validate,
     ),
 ):
     api = await kr8s.asyncio.api()
